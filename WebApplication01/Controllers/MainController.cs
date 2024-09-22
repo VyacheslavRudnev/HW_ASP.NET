2fb9bdf7716e28c7f2f326b423be0bd72cff4c77
Rudnev Vyacheslav <v.rudnev31@gmail.com>
2024-09-22 18:12:24 +0300

    new version

--- WebApplication01/Controllers/MainController.cs
+++ WebApplication01/Controllers/MainController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using WebApplication01.Data;
+using WebApplication01.Data.Entities;
+using WebApplication01.Models.Category;
 
 namespace WebApplication01.Controllers
 {
@@ -7,18 +9,60 @@ namespace WebApplication01.Controllers
     public class MainController : Controller
     {
 		private readonly AppBimbaDbContext _dbContext;
-		
-		//DI - dependency injection
-		public MainController(AppBimbaDbContext context)
+        //Зберігає різну інфорацію про MVC проект
+        private readonly IWebHostEnvironment _environment;   
+        //DI - dependency injection
+        public MainController(AppBimbaDbContext context,
+            IWebHostEnvironment environment)
 		{
 			_dbContext = context;
-		}
+            _environment = environment;
+        }
 
 		//метод у контролері називається Action - дія
 		public IActionResult Index()
         {
 			var model = _dbContext.Categories.ToList();
 			return View(model);
+        }
+		[HttpGet]//відображення сторінки для перегляду
+        public IActionResult Create()
+        {
+            //Ми повертає View - пусту, яка відобраєате сторінку де потрібно ввести дані для категорії
+            return View();
+        }
+        [HttpPost]//прийняття даних з форми
+        public IActionResult Create(CategoryCreateViewModel model)
+        {
+            var entity = new CategoryEntity();
+
+            //Збереження в базу даних
+            var dirName = "uploading";
+            var dirSave = Path.Combine(_environment.WebRootPath, dirName);
+            if (!Directory.Exists(dirSave))
+            {
+                Directory.CreateDirectory(dirSave);
+            }
+            if (model.Photo != null)
+            {
+                //унікальне значення для імені файлу, яке ніколи не повториться
+                string fileName = Guid.NewGuid().ToString();
+                var ext = Path.GetExtension(model.Photo.FileName);
+                fileName += ext;
+                var saveFile = Path.Combine(dirSave, fileName);
+                using (var stream = new FileStream(saveFile, FileMode.Create))
+                {
+                    model.Photo.CopyTo(stream);
+                }
+                entity.Image = fileName;
+            }
+            entity.Name = model.Name;
+            entity.Description = model.Description;
+            _dbContext.Categories.Add(entity);
+            _dbContext.SaveChanges();
+            //переходимо по списку всіх категорій, тобто визиваємо метод index для нашого контролера
+            return Redirect("/");
+           
         }
     }
 }
---
+++ WebApplication01/Models/Category/CategoryCreateViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication01.Models.Category;
+
+public class CategoryCreateViewModel
+{
+    [Display(Name = "Назва категорії")]
+    public string Name { get; set; } = String.Empty;
+    //тип для передачі файлів на сервер- зі сторінки хочу отримати файл із <input type="file"/>
+    [Display(Name = "Оберыть фото на ПК")]
+    public IFormFile? Photo { get; set; }
+    [Display(Name = "Короткий опис")]
+    public string Description { get; set; } = string.Empty;
+}
