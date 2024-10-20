8e1484abfe76147c463f3565a20bd747562198cf
Rudnev Vyacheslav <v.rudnev31@gmail.com>
2024-10-20 14:56:20 +0300

    add delete

--- WebApplication01/Controllers/ProductsController.cs
+++ WebApplication01/Controllers/ProductsController.cs
@@ -14,11 +14,13 @@ public class ProductsController : Controller
 {
     private readonly AppBimbaDbContext _dbContext;
     private readonly IMapper _mapper;
+    private readonly IImageWorker _imageWorker;
     //DI - Depencecy Injection
-    public ProductsController(AppBimbaDbContext context, IMapper mapper)
+    public ProductsController(AppBimbaDbContext context, IMapper mapper, IImageWorker imageWorker)
     {
         _dbContext = context;
         _mapper = mapper;
+        _imageWorker = imageWorker;
     }
     public IActionResult Index()
     {
@@ -47,6 +49,29 @@ public class ProductsController : Controller
         };
 
         return View(viewModel);
+    }
+
+    [HttpPost]
+    public IActionResult Delete(int id)
+    {
+        var product = _dbContext.Products.Include(x => x.ProductImages).SingleOrDefault(x => x.Id == id);
+        if (product == null)
+        {
+            return NotFound();
+        }
+        if (product.ProductImages != null)
+        {
+            foreach (var productImage in product.ProductImages)
+            {
+                _imageWorker.Delete(productImage.Image);
+                _dbContext.ProductImages.Remove(productImage);
+
+            }
+        }
+        _dbContext.Products.Remove(product);
+        _dbContext.SaveChanges();
+
+        return Json(new { text = "Ми його видалили" }); // Вертаю об'єкт у відповідь
     }
 
 
